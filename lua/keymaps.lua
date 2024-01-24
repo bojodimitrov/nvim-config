3fa3fa4055deaf832dd5fa4f6f080c054ec58a1a
VINikolov <v.nikolov.95@gmail.com>
2024-01-24 15:55:20 +0200

    feat: add some more remaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,10 +29,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- vim.keymap.set('v', '<leader>p', "\"_dP")
 vim.keymap.set('n', '<leader>p', "\"0p")
 vim.keymap.set('n', '<leader>P', "\"0P")
+
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+y")
+
+vim.keymap.set('n', '<leader>d', "\"_d")
+vim.keymap.set('v', '<leader>d', "\"_d")
+
 vim.keymap.set('n', 'J', "20j")
 vim.keymap.set('n', 'K', "20k")
+
+vim.keymap.set('n', '<leader>gs', '<Cmd>LazyGit<CR>')
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
+
 vim.keymap.set('n', '<C-d>', "<C-d>zz")
 vim.keymap.set('n', '<C-u>', "<C-u>zz")
-vim.keymap.set('n', '<leader>gs', '<Cmd>LazyGit<CR>')
+vim.keymap.set('n', 'n', "nzzzv")
+vim.keymap.set('n', 'N', "Nzzzv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- vim: ts=2 sts=2 sw=2 et
