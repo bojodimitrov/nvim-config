6605f6f2f7d83034e419a44ca9b951b9045a46be
VINikolov <v.nikolov.95@gmail.com>
2024-01-26 16:39:01 +0200

    feat: add the format-on-save plugin

---
+++ lua/custom/plugins/format-on-save.lua
@@ -0,0 +1,33 @@
+return {
+  "elentok/format-on-save.nvim",
+
+  config = function()
+    local formatters = require("format-on-save.formatters")
+    require("format-on-save").setup {
+      exclude_path_patterns = {
+        "/node_modules/",
+        ".local/share/nvim/lazy",
+      },
+
+      fallback_formatter = {
+        formatters.remove_trailing_newlines,
+        formatters.remove_trailing_whitespace
+      },
+
+      formatter_by_ft = {
+        css = formatters.lsp,
+        html = formatters.lsp,
+        javascript = formatters.lsp,
+        json = formatters.lsp,
+        lua = formatters.lsp,
+        markdown = formatters.prettierd,
+        python = formatters.black,
+        rust = formatters.lsp,
+        sh = formatters.shfmt,
+        typescript = formatters.prettierd,
+        typescriptreact = formatters.prettierd,
+        yaml = formatters.lsp,
+      },
+    }
+  end
+}
